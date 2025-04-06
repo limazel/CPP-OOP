26aa0bf149ab140e846c176676a3403963905145
Azad Ayhan <azadayhan50@gmail.com>
2025-04-06 17:44:57 +0300

    Pointer And Referance

--- inheritance.cpp
+++ inheritance.cpp
@@ -4,6 +4,11 @@ class Silah {
     public:
         std::string isim;
         int mermiKapasitesi;
+        std::string renk;
+
+        Silah() {
+            std::cout << "Silah yapıcı çağrıldı!" << std::endl;
+        }
 };
 
 class Ak47 : public Silah {
@@ -14,6 +19,10 @@ class Ak47 : public Silah {
 
         void atesEt() {
             std::cout << "Ateş ediliyor!" << std::endl;
+        }
+
+        Ak47() {
+            std::cout << "AK-47 yapıcı çağrıldı!" << std::endl;
         }
 };
 
@@ -39,10 +48,26 @@ class M1 : public Silah {
 
 };
 
+void test(Ak47& ak47, Ak47* ak47Ptr) {
+    Ak47* ak47Ptr2 = new Ak47();
+    Ak47& ak47Ref = *ak47Ptr2;
+}
+
 int main() {
 
     Ak47 ak47;
     ak47.isim = "Yeni Nesil AK-47";
+    ak47.renk = "Red";
+
+    Ak47* ak47Ptr = &ak47;
+    ak47Ptr->mermiKapasitesi = 30;
+    ak47Ptr->seriTek = true;
+
+    Ak47& ak47Ref = ak47;
+    ak47Ref.kayisUzunlugu = 100;
+
+    test(ak47, &ak47);
+    std::cout << "AK-47 Bilgileri:" << std::endl;
 
     return 0;
 }
